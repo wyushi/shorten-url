ef0ccd7cea1e7d9af3820d5c2106e03d6714cded
Yushi Wang <jasper.ys.wang@gmail.com>
2019-02-08 23:36:59 -0800

    add shorten url calculation algorithm

---
+++ server/src/utils/shorten.js
@@ -0,0 +1,14 @@
+const md5 = require('md5')
+const base62 = require("base62/lib/ascii")
+
+function hash(s) {
+  return md5(s)
+}
+
+function shorten(long) {
+  const hex = hash(long)
+  const b62 = base62.encode(parseInt(hex, 16))
+  return b62.substring(0, 3)
+}
+
+module.exports = shorten
